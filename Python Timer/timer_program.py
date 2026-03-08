3488db3dad5d6c3fe18de70b19cc8ae9aaf8edd0
Sai-sugash-tech <saisugash09@gmail.com>
2026-03-08 08:01:09 +0530

    Added Python Timer Program

---
+++ Python Timer/timer_program.py
@@ -0,0 +1,11 @@
+import time 
+import winsound
+mytime = int(input("enter time in seconds: "))
+for x in range(mytime, 0, -1):
+    sec = x % 60
+    min = int(x / 60) % 60
+    hrs = int(x / 3600)
+    print(f"{hrs:02}:{min:02}:{sec:02}")
+    time.sleep(1)
+print("TIMES UP....")
+winsound.Beep(100, 1000)
